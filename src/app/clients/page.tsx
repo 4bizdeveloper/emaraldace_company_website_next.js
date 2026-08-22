7ab16decc9acd70b038e25203372c0ab13a2bdd0
4bizdeveloper <4bizdeveloper@gmail.com>
2026-08-22 15:48:41 +0530

    today

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -42,7 +42,8 @@ const clients = [
   { name: "Trojan", url: "/client-logos/trojan.webp" },
   { name: "Winner Holistic Consultant", url: "/client-logos/winner-holistic-consultant.webp" },
   { name: "Infinity Drinks", url: "/client-logos/infinity-drinks-logo.png" },
-  { name: "Abrar", url: "/client-logos/abrar-logo.png" }
+  { name: "Abrar", url: "/client-logos/abrar-logo.png" },
+  { name: "Geco", url: "/client-logos/geco.webp" }
 ];
 export default function ClientsPage() {
   return (
